d21b91baa25ee9f964b15655825a579630213b3c
Nick Postma <n.postma@ccvshop.nl>
2017-11-10 15:00:54 +0100

    Small optimalisation where we have static and dynamic drawables

--- MRI/GUI.py
+++ MRI/GUI.py
@@ -1,40 +1,50 @@
 from Drawable import Rectangle, Point, Line, Label
 
+
 # Convert brain into a set of drawable objects
 class GUI:
     # Instance of the brain to scan
     brain = None
 
     # keep track of the location of a neuron (for drawing lines)
-    points = []
+    points = list()
 
     rectangles = list()
 
     lines = list()
 
-    strings = []
+    strings = list()
 
     canvas = None
 
     def __init__(self, brain, canvas):
 
+        # Static drawables
+        self.rectangles = list()
+        self.points = list()
+
+        # Dynmic drawables
+        self.lines = list();
+        self.strings = list();
+
         self.brain = brain
         self.canvas = canvas
         self.reset()
 
-    def update(self):
-        self.convertToDrawables();
+        self.createStaticDrawables()
 
-        self.canvas.reset();
+    def update(self):
+        self.updateDynamicDrawables()
+
+        self.canvas.reset()
         self.canvas.addRectanges(self.rectangles)
         self.canvas.addLines(self.lines)
         self.canvas.addStrings(self.getStrings())
-        self.canvas.repaint();
+        self.canvas.repaint()
 
     def reset(self):
-        self.rectangles = list()
         self.lines = list()
-        self.points = list()
+
 
     def getRectangles(self):
         return self.rectangles
@@ -43,19 +53,38 @@ class GUI:
         return self.lines
 
     def getStrings(self):
-        strings = list();
-        for(i, stringList) in enumerate(self.strings):
-            for(j, string) in enumerate(stringList):
+        strings = list()
+        for (i, stringList) in enumerate(self.strings):
+            for (j, string) in enumerate(stringList):
                 strings.append(string)
 
         return strings
 
-    def convertToDrawables(self):
+    def updateDynamicDrawables(self):
         self.reset()
-        self.convertRectangles()
-        self.convertLines()
+        self.createLines()
+        self.createStrings()
 
-    def convertRectangles(self):
+    def createStaticDrawables(self):
+        self.createRectangles()
+
+    def createStrings(self):
+        for (layerNr, layer) in enumerate(self.brain.layers):
+
+            if (layerNr >= len(self.strings)):
+                self.strings.insert(layerNr, [])
+
+            for (neuronNr, neuron) in enumerate(layer.neurons):
+                centerPoint = self.points[layerNr][neuronNr]
+                # String objects are not removed. Only updated if available
+                # TODO: If this is faster then removing / creating then rewrite for Rect/Lines
+                # TODO: Implement metrics to prove performance improvements
+                if (neuronNr >= len(self.strings[layerNr])):
+                    self.strings[layerNr].insert(neuronNr, Label.Label(centerPoint, str(neuron.value)))
+                else:
+                    self.strings[layerNr][neuronNr] = Label.Label(centerPoint, str(neuron.value))
+
+    def createRectangles(self):
         # Editable vars
         layerXOffset = 10
         layerMargin = 60
@@ -90,25 +119,14 @@ class GUI:
             if (layerNr >= len(self.points)):
                 self.points.insert(layerNr, [])
 
-            if (layerNr >= len(self.strings)):
-                self.strings.insert(layerNr, [])
-
             self.rectangles.append(rect)
 
             for (neuronNr, neuron) in enumerate(layer.neurons):
                 x = (layerPosition * (layerWidth + layerMargin)) + neuronXOffset
                 y = 15 + (neuronNr * layerHeightSteps) + yOffset
 
-                centerPoint =  Point.Point(x + (neuronWidth / 2), y + (neuronHeight / 2))
+                centerPoint = Point.Point(x + (neuronWidth / 2), y + (neuronHeight / 2))
                 self.points[layerNr].insert(neuronNr, centerPoint)
-
-                # String objects are not removed. Only updated if available
-                # TODO: If this is faster then removing / creating then rewrite for Rect/Lines
-                # TODO: Implement metrics to prove performance improvements
-                if (neuronNr >= len(self.strings[layerNr])):
-                    self.strings[layerNr].insert(neuronNr, Label.Label(centerPoint, str(neuron.value)))
-                else :
-                    self.strings[layerNr][neuronNr] = Label.Label(centerPoint, str(neuron.value))
 
                 rect = Rectangle.Rectangle(
                     x,
@@ -119,7 +137,7 @@ class GUI:
 
                 self.rectangles.append(rect)
 
-    def convertLines(self):
+    def createLines(self):
         for (layerNr, layer) in enumerate(self.brain.layers):
             for (neuronNr, neuron) in enumerate(layer.neurons):
                 for (weightNr, weight) in enumerate(neuron.weights):
